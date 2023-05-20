ad1dbaa695df6a09abe18f7274fd8bde7127a8c5
Tejas Lamba <tejas22feb@gmail.com>
2023-05-20 20:05:04 +0530

    Refactor help command implementation for readability and organization.

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -39,126 +39,100 @@ module.exports = {
   argstoomany_message: "", //Message if the user has too many / not enough args / too many plus args, which will be sent, leave emtpy / dont add, if you wanna use command.usage or the default message! [OPTIONAL]
   slashRun: async (client, interaction) => {
     try {
-      //things u can directly access in an interaction!
       const {
         member,
-        channelId,
-        guildId,
-        applicationId,
-        commandName,
-        deferred,
-        replied,
-        ephemeral,
         options,
-        id,
-        createdTimestamp,
       } = interaction;
-      const { guild } = member;
-      let prefix = client.config.prefix;
-      let args = options.getString("specific_cmd");
-      if (args && args.length > 0) {
-        const embed = new EmbedBuilder();
-        const cmd =
-          client.commands.get(args.toLowerCase()) ||
-          client.commands.get(client.aliases.get(args.toLowerCase()));
-        if (!cmd) {
-          const allcmds = client.commands.map((cmd) => cmd.name);
-          const matches = stringSimilarity.findBestMatch(
-            args.toLowerCase(),
-            allcmds
-          );
-          const suggestions = matches.ratings
-    .map((rating, index) => ({
-      command: allcmds[index],
-      rating: rating.rating
-    }))
-    .filter(suggestion => suggestion.rating > 0);
-          const suggestionList = suggestions
-          .map((suggestion, index) => `\`[${index + 1}]\` ${suggestion.command}`)
-          .join('\n');
       
-        return interaction.reply({
-          ephemeral: true,
-          embeds: [
-            embed.setColor(ee.wrongcolor).setDescription(
-              `No Information found for command **${args.toLowerCase()}** \nDo you mean this? \n> ${suggestionList}`
-            ),
-          ],
+      const { guild } = member;
+      const prefix = client.config.prefix;
+      const args = options.getString("specific_cmd");
+    
+      if (args && args.length > 0) {
+        const commandName = args.toLowerCase();
+        const cmd = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
+    
+        if (!cmd) {
+          const allCommands = client.commands.map(cmd => cmd.name);
+          const matches = stringSimilarity.findBestMatch(commandName, allCommands);
+          const suggestions = matches.ratings
+            .map((rating, index) => ({
+              command: allCommands[index],
+              rating: rating.rating
+            }))
+            .filter(suggestion => suggestion.rating > 0);
+    
+          const suggestionList = suggestions
+            .map((suggestion, index) => `\`[${index + 1}]\` ${suggestion.command}`)
+            .join('\n');
+    
+          const errorMessage = `No information found for command **${commandName}**\nDo you mean this?\n> ${suggestionList}`;
+    
+          return interaction.reply({
+            ephemeral: true,
+            embeds: [new EmbedBuilder().setColor(ee.wrongcolor).setDescription(errorMessage)],
           });
         }
-        if (cmd.name)
-          embed.addFields({
-            name: "**Command name**",
-            value: `\`${cmd.name}\``,
-          });
-        if (cmd.name)
-          embed.setTitle(`Detailed Information about:\`${cmd.name}\``);
+    
+        const embed = new EmbedBuilder()
+          .setColor(ee.color)
+          .setTitle(`Detailed Information about: \`${cmd.name}\``);
+    
         if (cmd.aliases)
           embed.addFields({
             name: "**Aliases**",
-            value: `\`${cmd.aliases.map((a) => `${a}`).join("`, `")}\``,
+            value: cmd.aliases.map(alias => `\`${alias}\``).join(", "),
           });
+    
         embed.addFields({
           name: "**Cooldown**",
-          value: `\`${
-            cmd.cooldown
-              ? cmd.cooldown + " Seconds"
-              : settings.default_cooldown_in_sec + " Second"
-          }\``,
+          value: cmd.cooldown ? `\`${cmd.cooldown} Seconds\`` : `\`${settings.default_cooldown_in_sec} Second\``,
         });
-        if (cmd.usage) {
+    
+        if (cmd.usage)
           embed.addFields({
             name: "**Usage**",
             value: `\`${prefix}${cmd.usage}\``,
           });
-          embed.setFooter({ text: ee.footertext, iconURL: ee.footericon });
-        }
+    
         embed.setDescription(`${codeBlock("diff", `- [] = optional argument
-- <> = required argument
-- Do NOT type these when using commands!`)}
-> ${cmd.description}
-`)
+    - <> = required argument
+    - Do NOT type these when using commands!`)}
+    > ${cmd.description}`);
+    
         return interaction.reply({
           ephemeral: true,
-          embeds: [embed.setColor(ee.color)],
+          embeds: [embed],
         });
       } else {
-        const commands = (category) => {
+        const commandsByCategory = (category) => {
           return client.slashCommands
-            .filter((cmd) => cmd.category === category)
-            .map((cmd) => `\`${cmd.name}\``);
+            .filter(cmd => cmd.category === category)
+            .map(cmd => `\`${cmd.name}\``);
         };
+    
         const embed = new EmbedBuilder()
           .setColor(ee.color)
           .setThumbnail(client.user.displayAvatarURL())
           .setTitle("HELP MENU 🔰 Commands")
           .setDescription(
             `- Prefix for this server is \`${prefix}\`
-- Total commands: \`${client.commands.size}\`
-- [Get ${client.config.botName}](${client.config.invite_url}) | [Support server](${client.config.support_server.invite}) | [Vote me](https://top.gg/bot/${client.user.id}/vote)
-- Type \`${prefix}help <command | module>\` for more info.`
+    - Total commands: \`${client.commands.size}\`
+    - [Get ${client.config.botName}](${client.config.invite_url}) | [Support server](${client.config.support_server.invite}) | [Vote me](https://top.gg/bot/${client.user.id}/vote)
+    - Type \`${prefix}help <command | module>\` for more info.`
           )
           .setFooter({
             text: `To see command Descriptions and Information, type: ${prefix}help [CMD NAME]`,
             iconURL: client.user.displayAvatarURL(),
           });
-        
-        let options = [];
-        try {
-         
-          for (let i = 0; i < client.categories.length; i += 1) {
-            const current = client.categories[i];
-            const items = commands(current);
-            options.push({
-              label: current,
-              value: current.toLowerCase(),
-              description: `Commands count ${items.length}`,
-              emoji: client.allEmojis[current.toLowerCase()] ? client.allEmojis[current.toLowerCase()] : undefined,
-            });
-          }
-        } catch (e) {
-          console.log(String(e.stack).red);
-        }
+    
+        const options = client.categories.map(category => ({
+          label: category,
+          value: category.toLowerCase(),
+          description: `Commands count: ${commandsByCategory(category).length}`,
+          emoji: client.allEmojis[category.toLowerCase()] || undefined,
+        }));
+    
         const msg = await interaction.reply({
           ephemeral: true,
           embeds: [embed],
@@ -173,11 +147,13 @@ module.exports = {
             ),
           ],
         });
-        const filter_menu = (interaction) => interaction.customId === 'help_menu_select';
-      let response
+    
+        const filterMenu = interaction => interaction.customId === 'help_menu_select';
+        let collector;
+    
         try {
           collector = msg.createMessageComponentCollector({
-            filter_menu,
+            filter: filterMenu,
             time: 30_000
           });
         } catch (error) {
@@ -189,30 +165,34 @@ module.exports = {
             ],
           });
         }
+    
         collector.on('collect', async (response) => {
-        if(response.customId === 'help_menu_select') {
-          const cat = response.values[0]
-          const cat_embed = new EmbedBuilder()
-          const items = commands(capitalizeFirstLetter(cat));
-          cat_embed.setDescription(`**__${capitalizeFirstLetter(cat.toUpperCase())}__ [${items.length}]**\n \n - \`${items.join("\`, \`")}\``);
-          interaction.editReply({
-            embeds: [cat_embed],
-          })
-          response.deferUpdate();
-        }
-      })
-      collector.on('end', async (response) => {
-        return interaction.editReply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor(ee.color)
-              .setDescription(`${emote.x} | You ran out of time! Try again!`),
-          ],
+          if (response.customId === 'help_menu_select') {
+            const selectedCategory = response.values[0];
+            const categoryCommands = commandsByCategory(capitalizeFirstLetter(selectedCategory));
+            const categoryEmbed = new EmbedBuilder()
+              .setDescription(`**__${capitalizeFirstLetter(selectedCategory.toUpperCase())}__ [${categoryCommands.length}]**\n \n - ${categoryCommands.join(", ")}`);
+    
+            interaction.editReply({
+              embeds: [categoryEmbed],
+            });
+    
+            response.deferUpdate();
+          }
         });
-      });
+    
+        collector.on('end', async (response) => {
+          return interaction.editReply({
+            embeds: [
+              new EmbedBuilder()
+                .setColor(ee.color)
+                .setDescription(`${emote.x} | You ran out of time! Try again!`),
+            ],
+          });
+        });
       }
-    } catch (e) {
-      console.log(String(e.stack).bgRed);
+    } catch (error) {
+      console.log(String(error.stack).bgRed);
       return interaction.followUp({
         ephemeral: true,
         embeds: [
@@ -220,16 +200,11 @@ module.exports = {
             .setColor(ee.wrongcolor)
             .setFooter({ text: ee.footertext, iconURL: ee.footericon })
             .setTitle(`${client.allEmojis.x} ERROR | An error occurred`)
-            .setDescription(
-              `\`\`\`${
-                e.message
-                  ? String(e.message).substr(0, 2000)
-                  : String(e).substr(0, 2000)
-              }\`\`\``
-            ),
+            .setDescription(`\`\`\`${String(error.message || error).substr(0, 2000)}\`\`\``),
         ],
       });
     }
+    
   },
   messageRun: async (
     client,
