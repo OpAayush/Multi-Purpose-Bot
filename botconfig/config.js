c5541567af9b698845a0bf15d0e97cfa2ccabfdf
Tejas Lamba <tejas22feb@gmail.com>
2023-05-20 17:03:13 +0530

    Add bot prefix for commands and fix channel partialfetch issue
    This commit adds the bot prefix for commands in the `config.js` file and fixes the issue causing partial fetch errors in the `messageCreate.js` file. It also includes various improvements to the command execution process such as checking for cooldowns, required permissions, and max/min arguments.

--- botconfig/config.js
+++ botconfig/config.js
@@ -2,6 +2,7 @@ module.exports = {
   "token": process.env.TOKEN, //Discord Bot Token
   "MongoURL": "mongodb+srv://xd666:naveen123@cluster.gbcmjjt.mongodb.net/?retryWrites=true&w=majority", //if dbType = MONGO, this is required else skip
   "loadSlashsGlobal": true,
+  "prefix": "!", //Bot Prefix
   "dirSetup": [{
     "Folder": "Info", "CmdName": "info",
     "CmdDescription": "Grant specific Information about something!"
